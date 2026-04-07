482e2f31ae7cfadbc323d96fc9eb13602a1f8071
sanket2323 <sanketmane2323@gmail.com>
2026-04-07 23:14:13 +1000

    task 1 and task 2 implemented

---
+++ a1.py
@@ -0,0 +1,28 @@
+# DO NOT modify or add any import statements
+from support import *
+
+ALL_WORDS = load_words("words.txt")
+
+# Name: Sanket Mane
+# Student Number: 50040467
+# Favorite Word: 
+# -----------------------------------------------------------------------------
+
+# Define your functions here
+# task 1
+def num_hours() -> float :
+    return 3.14
+
+#task 2 
+def has_won(guess: str , target : str ) -> bool:
+    if(guess == target):
+        return True
+    return False
+
+# task 3
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
