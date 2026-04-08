a5d3b919630615dd40b40fdd652637ee3db665ec
sanket2323 <sanketmane2323@gmail.com>
2026-04-08 10:18:49 +1000

    task 3 implemented

--- a1.py
+++ a1.py
@@ -3,6 +3,7 @@ from support import *
 
 ALL_WORDS = load_words("words.txt")
 
+
 # Name: Sanket Mane
 # Student Number: 50040467
 # Favorite Word: 
@@ -10,16 +11,25 @@ ALL_WORDS = load_words("words.txt")
 
 # Define your functions here
 # task 1
-def num_hours() -> float :
+def num_hours() -> float:
     return 3.14
 
-#task 2 
-def has_won(guess: str , target : str ) -> bool:
-    if(guess == target):
+# task 2
+def has_won(guess: str, target: str) -> bool:
+    if guess == target:
         return True
     return False
 
 # task 3
+def get_max_guesses() -> int:
+    while True:
+        number_of_guesses = input(
+            "\nPlease enter the number of guesses you require to guess the secret word. Your input must be a number between 5 and 9: ")
+        if number_of_guesses.isdigit():
+            number_of_guesses = int(number_of_guesses)
+            if number_of_guesses <= 9 and number_of_guesses >= 5:
+                break
+    return number_of_guesses
 
 def main() -> None:
     pass
