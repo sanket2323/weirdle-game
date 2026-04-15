00659e0dd99e2a50212a16de440efeeaf9e87267
sanket2323 <sanketmane2323@gmail.com>
2026-04-15 21:02:26 +1000

    doc string added

--- a1.py
+++ a1.py
@@ -12,11 +12,24 @@ ALL_WORDS = load_words("words.txt")
 # Define your functions here
 # task 1
 def num_hours() -> float:
-    return 3.14
+    """
+    Returns the number of hours spent on the assignment
+    Returns:
+    (float) : Number of hours worked
+    """
+    return 4.14
 
 
 # task 2
 def has_won(guess: str, target: str) -> bool:
+    """
+    checks if the guess matches the target word
+    Parameter:
+        guess: the guess word
+        target: the target word
+    Returns:
+    (boolean) : True if the guess matches the target word, otherwise False
+    """
     if guess == target:
         return True
     return False
@@ -24,6 +37,11 @@ def has_won(guess: str, target: str) -> bool:
 
 # task 3
 def get_max_guesses() -> int:
+    """
+    Gets the maximum number of guesses allowed between 5 and 9
+    Returns:
+    (int) : A valid number of guesses between 5 and 9 entered by user.
+    """
     while True:
         number_of_guesses = input(GET_NUM_GUESSES_MESSAGE)
         if number_of_guesses.isdigit():
@@ -35,6 +53,14 @@ def get_max_guesses() -> int:
 
 # task 4
 def create_board(max_guesses: int) -> list[tuple[str, str]]:
+    """
+    Create an empty game board
+    Parameter:
+        max_guesses: Number of guesses allowed
+
+    Returns:
+    A list of tuples representing the game board
+    """
     # create empty row value
     empty_row_value = EMPTY * 6
 
@@ -53,6 +79,14 @@ def create_board(max_guesses: int) -> list[tuple[str, str]]:
 
 # task 5
 def display_board(board: list[tuple[str, str]]) -> None:
+    """
+    Display the game board
+    Parameter:
+        board: The game board
+
+    Returns:
+    None
+    """
     print(SEP)
     for i in range(len(board)):
         print(f"Guess {i + 1}:  {board[i][0]}")
@@ -62,16 +96,28 @@ def display_board(board: list[tuple[str, str]]) -> None:
 
 # task 6
 def generate_secret_word() -> str:
+    """
+    Select a random word from the list of words
+    Returns:
+    A random word chosen from list of words
+    """
     index = randint(0, len(ALL_WORDS) - 1)
     return ALL_WORDS[index]
 
 
 # task 7
 def validate_input(command: str) -> bool:
-    # valid single-letter special commands
-    valid_list = ["h","H","a","A","q","Q"]
+    """
+    checks if the command entered is valid
+    Parameter:
+        command: The user input
 
-    if command in valid_list:
+    Returns:
+    True if command is valid, otherwise False
+    """
+    # valid single-letter special commands
+
+    if command in HELP_COMMAND or command in QUIT_COMMAND or command in KEYBOARD_COMMAND:
         return True
 
     # guess must be exactly 6 alphabetic characters
@@ -84,6 +130,7 @@ def validate_input(command: str) -> bool:
         print(INVALID_CHARACTERS_MESSAGE)
         return False
 
+    # check if command belong to list of words provided
     if command not in ALL_WORDS:
         print(INVALID_GUESS_MESSAGE)
         return False
@@ -92,6 +139,11 @@ def validate_input(command: str) -> bool:
 
 # task 8
 def get_command() -> str:
+    """
+    Get a command from the user
+    Returns:
+    A valid command
+    """
     while True:
         command = input(ENTER_COMMAND_MESSAGE)
         if validate_input(command):
@@ -101,6 +153,15 @@ def get_command() -> str:
 
 # task 9
 def get_feedback(guess: str, target: str) -> str:
+    """
+    Generate feedback for a guess
+    Parameters:
+        guess: The guessed word
+        target: The secret word
+
+    Returns:
+    A string representing feedback.
+    """
     feedback = ""
     for i in range(6):
         if guess[i] == target[i]:
@@ -114,6 +175,17 @@ def get_feedback(guess: str, target: str) -> str:
 
 # task 10
 def update_board(board: list[tuple], guess_num: int, guess: str, target: str) -> None:
+    """
+    Update the board with a new guess and feedback
+    Parameters:
+        board: the game board
+        guess_num: current guess number
+        guess: the guessed word
+        target: the secret word
+
+    Returns:
+    None
+    """
     get_feedback_from_fun = get_feedback(guess, target)
     board[guess_num - 1] = (guess, get_feedback_from_fun)
     return None
@@ -121,6 +193,14 @@ def update_board(board: list[tuple], guess_num: int, guess: str, target: str) ->
 
 # task 11
 def display_keyboard(keyboard: dict[str, str]) -> None:
+    """
+    Display the keyboard
+    Parameter:
+        keyboard:Dictionary storing letter statuses
+
+    Returns:
+    None
+    """
     print("Keyboard:")
     print(SEP)
     items = list(keyboard.items())
@@ -134,6 +214,16 @@ def display_keyboard(keyboard: dict[str, str]) -> None:
 
 # task 12
 def update_keyboard(board: list[tuple], keyboard: dict[str, str], guess_num: int) -> None:
+    """
+    Update the keyboard based on feedback
+    Parameters:
+        board:the game board
+        keyboard: the keyboard dictionary
+        guess_num: current guess number
+
+    Returns:
+    None
+    """
     guess_word = board[guess_num - 1][0]
     feedback_word = board[guess_num - 1][1]
 
@@ -161,6 +251,11 @@ def update_keyboard(board: list[tuple], keyboard: dict[str, str], guess_num: int
 
 # task 13
 def play_game() -> None:
+    """
+    Runs one full game Weirdle.
+    Returns:
+    None
+    """
     print(WELCOME_MESSAGE)
     sec_word = generate_secret_word()
     max_guesses = get_max_guesses()
@@ -201,6 +296,11 @@ def play_game() -> None:
 
 # task 14
 def main() -> None:
+    """
+    Runs the game repeatedly until users quits.
+    Returns:
+    None
+    """
     while True:
         play_game()
         retry_game_or_not = input(RETRY_MESSAGE)
